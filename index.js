e90280d1458f4b08e0af044ceba60db9461a79a4
CMotillon <cedricmotillon@outlook.com>
2021-04-22 17:23:55 -0400

    Commit

--- index.js
+++ index.js
@@ -19,20 +19,54 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
       axios.get('https://rocket-elevators-cm.azurewebsites.net/api/Elevators'),
       axios.get('https://rocket-elevators-cm.azurewebsites.net/api/Buildings'),
       axios.get('https://rocket-elevators-cm.azurewebsites.net/api/Buildings/Customers'),
-      axios.get('https://rocket-elevators-cm.azurewebsites.net/api/Buildings/Quotes')
+      axios.get('https://rocket-elevators-cm.azurewebsites.net/api/Buildings/Quotes'),
+      axios.get('https://rocket-elevators-cm.azurewebsites.net/api/Batteries'),
+      axios.get('https://rocket-elevators-cm.azurewebsites.net/api/Elevators/statuscheck'),
+      axios.get('https://rocket-elevators-cm.azurewebsites.net/api/Buildings/Cities')
+      
     ]).then(responseArr => {
       const nb_elevators = responseArr[0].data.length;
       const nb_buildings = responseArr[1].data.length;
       const nb_customers = responseArr[2].data.length;
       const nb_quotes = responseArr[3].data.length;
+      const nb_batteries = responseArr[4].data.length;
+      const nb_bad_elevators = responseArr[5].data.length;
+      const nb_cities = responseArr[6].data.length;
 
-      agent.add(`Greetings. There are currently ${nb_elevators} deployed in the ${nb_buildings} buildings of your ${nb_customers} customers. On another note you currently have ${nb_quotes} quotes awaiting processing.`);
+      agent.add(`Greetings. 
+        There are currently ${nb_elevators} deployed in the ${nb_buildings} buildings of your ${nb_customers} customers.\n
+        Currently, ${nb_bad_elevators} elevators are not in Running Status and are being serviced. \n
+        ${nb_batteries} Batteries are deployed across ${nb_cities} cities. \n
+        On another note you currently have ${nb_quotes} quotes awaiting processing.`);
     });
   }
  
   function fallback(agent) {
     agent.add(`I didn't understand`);
     agent.add(`I'm sorry, can you try again?`);
+  }
+  
+  function status(agent) {
+    const id = agent.parameters.id;
+    var controller = agent.parameters.controller;
+    switch(controller){
+      case "elevator":
+        controller = "elevators";
+        break;
+      case "battery":
+        controller = "batteries";
+        break;
+      case "column":
+        controller = "columns";
+        break;
+      default:
+        controller = "elevators";
+    }
+    const url = "https://rocket-elevators-cm.azurewebsites.net/api/" + controller + "/" + id;
+    axios.get(url).then(response => {
+      console.log(response.data);
+      agent.add(`This is a test`);
+  	});
   }
 
   // // Uncomment and edit to make your own intent handler
@@ -68,6 +102,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   let intentMap = new Map();
   intentMap.set('Default Welcome Intent', welcome);
   intentMap.set('Default Fallback Intent', fallback);
+  intentMap.set('Status Intent', status);
   // intentMap.set('your intent name here', yourFunctionHandler);
   // intentMap.set('your intent name here', googleAssistantHandler);
   agent.handleRequest(intentMap);
